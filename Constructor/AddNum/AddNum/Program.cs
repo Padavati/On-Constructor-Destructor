50769aa4734f9cd21beec09e616a69c0ca3e3b41
Padavati <lavanyavaralu07399@gmail.com>
2021-12-30 15:41:08 +0530

    File added

---
+++ Constructor/AddNum/AddNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Constructor ctr1= new Constructor();
+            Constructor ctr2 = new Constructor("I'm a next Parameterized msg");
+            Constructor ctr3 = new Constructor(5,15);
+
+            Console.ReadKey();
+        }
+    }
+    public class Constructor
+    {
+        public Constructor()
+        {
+            Console.WriteLine("I'm a default constructor");
+        }
+        public Constructor(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public Constructor(int num1,int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("Adding of two numbers:" + sum);
+        }
+    }
+
+}
---
+++ Constructor/Constructor Overloading/Constructor Overloading/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserDetails us = new UserDetails();
+            UserDetails us1 = new UserDetails("Lavanya", 21);
+
+            Console.ReadKey();
+        }
+    }
+    public class UserDetails
+    {
+        string user;
+        int age;
+        //Default Constructor
+        public UserDetails()
+        {
+            user = "Puneeth";
+            age = 22;
+            Console.WriteLine("Previous User {0} and he is {1} years old", user, age);
+        }
+        //Parameterized Constructor
+        public UserDetails(string name, int n)
+        {
+            user = name;
+            age = n;
+            Console.WriteLine("Current User {0} and she is {1} years old", user, age);
+        }
+         ~UserDetails()
+        {
+            Console.WriteLine("I'm a Destructor and i clean  the resources and free up memory as soon as prgm closed"); 
+        }
+    }
+}
+
+
